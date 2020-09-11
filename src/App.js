64df6e3a51c0016f698fcb80d63cef47f44fe330
zouzonghua <zouzonghua0511@gmail.com>
2020-09-11 15:53:19 +0800

    test: 测试环境变量是否配置成功

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import TodoList from './containers/TodoList'
 
 function App() {
+  const env = process.env.REACT_APP_API_URL
+  console.log('env:', env)
   return (
     <div>
       <TodoList />
