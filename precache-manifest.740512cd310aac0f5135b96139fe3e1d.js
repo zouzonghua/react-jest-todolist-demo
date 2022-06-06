5dff68f37fe8c5dc4c0cc8f7ebda8f0f6d611ee4
zouzonghua <zouzonghua.cn@gmail.com>
2022-06-06 13:41:58 +0000

    Deploying to gh-pages from @ zouzonghua/react-jest-todolist-demo@380fbd117dc52712ff6e65c4338f47271a65de5a 🚀

---
+++ precache-manifest.740512cd310aac0f5135b96139fe3e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f76177a05f277fdd671a5694a2aa295",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac2e6d0528bc34adfbae",
+    "url": "./static/css/main.75f7c326.chunk.css"
+  },
+  {
+    "revision": "95350eb2b1e18da8ddbb",
+    "url": "./static/js/2.f3557cc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f3557cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2e6d0528bc34adfbae",
+    "url": "./static/js/main.d875948b.chunk.js"
+  },
+  {
+    "revision": "00499514c2749169c710",
+    "url": "./static/js/runtime-main.417613da.js"
+  }
+]);
